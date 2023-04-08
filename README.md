8a43a8e72df7fd0fe275f11ae4ea2cfe350e29c6
BBee <atsushi.kokubun.47@gmail.com>
2023-04-08 16:16:11 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# AsseemblePC_AndroidApp
+<pre>
+Assemble PC Android app built using Jetpack Compose.
+This app is useful for selecting parts for your own PC and estimating the total cost.
+It is still in development and not yet published on the PlayStore.
+</pre>
---
+++ app/src/main/java/jp/developer/bbee/assemblepc/MainActivity.kt
@@ -0,0 +1,26 @@
+package jp.developer.bbee.assemblepc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import jp.developer.bbee.assemblepc.ui.theme.AssemblePCTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AssemblePCTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                }
+            }
+        }
+    }
+}
