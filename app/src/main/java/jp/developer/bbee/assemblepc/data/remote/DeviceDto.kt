579474852fb33b82d2d4bc8dcca951656c7a78ff
BBee <atsushi.kokubun.47@gmail.com>
2023-04-08 20:13:40 +0900

    Extention Dto to model

--- app/src/main/java/jp/developer/bbee/assemblepc/data/remote/DeviceDto.kt
+++ app/src/main/java/jp/developer/bbee/assemblepc/data/remote/DeviceDto.kt
@@ -3,6 +3,7 @@ package jp.developer.bbee.assemblepc.data.remote
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import jp.developer.bbee.assemblepc.domain.model.Device
 
 @JsonClass(generateAdapter = true)
 data class DeviceDto(
@@ -26,3 +27,29 @@ data class Result(
     val releasedate: String?,
     val url: String?
 )
+
+fun DeviceDto.toDevice(): List<Device> {
+    if (results == null) return emptyList()
+    return results.map {
+        Device(
+            id = it.id!!,
+            device = it.device!!,
+            name = it.name ?: "",
+            imgurl = it.imgurl ?: "https://img1.kakaku.k-img.com/images/productimage/l/nowprinting.gif",
+            invisible = when (it.invisible) {
+                null -> true
+                1 -> true
+                else -> false
+            },
+            url = it.url ?: "https://kakaku.com/",
+            detail = it.detail ?: "",
+            price = it.price ?: 0,
+            rank = it.rank ?: 99,
+            releasedate = it.releasedate ?: "20000101",
+            flag1 = it.flag1,
+            flag2 = it.flag2,
+            createddate = it.createddate,
+            lastupdate = it.lastupdate
+        )
+    }
+}
