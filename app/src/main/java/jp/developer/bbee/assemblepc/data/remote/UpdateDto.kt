e7aaf16c7bce7534b75ce0d0b5d84c8bda9e5b68
BBee <atsushi.kokubun.47@gmail.com>
2023-04-16 17:34:33 +0900

    Add Update api and dto

---
+++ app/src/main/java/jp/developer/bbee/assemblepc/data/remote/UpdateDto.kt
@@ -0,0 +1,14 @@
+package jp.developer.bbee.assemblepc.data.remote
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class UpdateDto(
+    val kakakuupdate: Int?
+)
+
+fun UpdateDto.toIntUpdate(): Int {
+    return kakakuupdate ?: 0
+}
