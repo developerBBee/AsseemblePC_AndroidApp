f3889f338d1087b8a2e60a8bcf5d347ffb24b6ff
BBee <atsushi.kokubun.47@gmail.com>
2023-04-29 15:20:55 +0900

    Fix overflow text in AssemblyInfo

--- app/src/main/java/jp/developer/bbee/assemblepc/presentation/device/components/AssemblyInfo.kt
+++ app/src/main/java/jp/developer/bbee/assemblepc/presentation/device/components/AssemblyInfo.kt
@@ -1,11 +1,16 @@
 package jp.developer.bbee.assemblepc.presentation.device.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import jp.developer.bbee.assemblepc.presentation.device.AssemblyViewModel
 
@@ -15,10 +20,27 @@ fun AssemblyInfo(
 ) {
     val prices = assemblyViewModel.assemblies.value.sumOf { it.devicePriceRecent }
     val isNoPrice = assemblyViewModel.assemblies.value.any { it.devicePriceRecent == 0 }
-    Row (modifier = Modifier.fillMaxWidth()){
-        Text(text = "構成名称:${assemblyViewModel.selectedAssemblyName.value}")
-        Spacer(modifier =Modifier.weight(1f))
+    Row (
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ){
+        Text(
+            modifier = Modifier.width(0.dp).weight(1f),
+            text = "構成名:${assemblyViewModel.selectedAssemblyName.value}",
+            fontWeight = FontWeight.ExtraBold,
+            maxLines = 1,
+            softWrap = false,
+            overflow = TextOverflow.Ellipsis,
+            textAlign = TextAlign.Start
+        )
         // TODO: 価格のインクリメントアニメーションを追加したい
-        Text(text = "合計金額　¥ ${"%,d".format(prices) + if (isNoPrice) "(＋α)" else ""}")
+        Text(
+            text = " 合計 ¥ ${"%,d".format(prices) + if (isNoPrice) "(+α)" else ""}",
+            fontWeight = FontWeight.ExtraBold,
+            maxLines = 1,
+            softWrap = false,
+            overflow = TextOverflow.Visible,
+            textAlign = TextAlign.End
+        )
     }
 }
