2f8a36f31386b364359f36f7c55c94125fdfdd94
BBee <atsushi.kokubun.47@gmail.com>
2023-04-29 16:19:49 +0900

    Modify view of TopScreen

--- app/src/main/java/jp/developer/bbee/assemblepc/presentation/top/TopScreen.kt
+++ app/src/main/java/jp/developer/bbee/assemblepc/presentation/top/TopScreen.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,16 +29,14 @@ fun TopScreen(
 ) {
     val showDialogState = remember { mutableStateOf(false) }
 
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
     ) {
         // Temporary code
         LazyColumn(
-            modifier = Modifier
-                .weight(0.9f)
-                .fillMaxWidth(),
+            modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             items(topViewModel.allAssemblyList) {
@@ -52,30 +52,26 @@ fun TopScreen(
                     }
                 )
             }
-        }
-        Column(
-            modifier = Modifier
-                .weight(0.1f)
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Button(
-                onClick = { showDialogState.value = true },
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = Color(0xFFe0ffff)
-                ),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 10.dp)
-            ) {
-                Text(
-                    text = "構成を新規作成",
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color(0xff2f4f4f)
-                )
+            if (topViewModel.allAssemblyList.size > 2) {
+                item { Spacer(modifier = Modifier.height(50.dp)) }
             }
+        }
+        Button(
+            onClick = { showDialogState.value = true },
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color(0xFFe0ffff)
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(vertical = 10.dp)
+        ) {
+            Text(
+                text = "構成を新規作成",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xff2f4f4f)
+            )
         }
     }
     if (showDialogState.value) {
