aa42c29f1c636cace257f0176b65773cd68d05e8
BBee <atsushi.kokubun.47@gmail.com>
2023-05-04 12:57:03 +0900

    Add loading indicator

--- app/src/main/java/jp/developer/bbee/assemblepc/presentation/device/DeviceScreen.kt
+++ app/src/main/java/jp/developer/bbee/assemblepc/presentation/device/DeviceScreen.kt
@@ -5,10 +5,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import jp.developer.bbee.assemblepc.R
 import jp.developer.bbee.assemblepc.presentation.device.components.AddAssemblyDialog
 import jp.developer.bbee.assemblepc.presentation.device.components.AssemblyInfo
 import jp.developer.bbee.assemblepc.presentation.device.components.DeviceRow
@@ -23,17 +28,32 @@ fun DeviceScreen(
     val state = deviceViewModel.state.value
     Column(modifier = Modifier.fillMaxSize()) {
         AssemblyInfo()
-        LazyColumn(modifier = Modifier
-            .weight(1f)
-            .fillMaxWidth()) {
-            items(state.devices) { device ->
-                DeviceRow(device = device) {
-                    assemblyViewModel.selectedDevice = it
-                    assemblyViewModel.isShowDialog = true
+        when {
+            state.isLoading -> {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
+            }
+            !state.error.isNullOrBlank() -> {
+                Text(
+                    text = "${stringResource(id = R.string.network_err_msg)}\n\n${state.error}",
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                )
+            }
+            else -> {
+                LazyColumn(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxWidth()
+                ) {
+                    items(state.devices) { device ->
+                        DeviceRow(device = device) {
+                            assemblyViewModel.selectedDevice = it
+                            assemblyViewModel.isShowDialog = true
+                        }
+                    }
                 }
+                DeviceSearchText()
             }
         }
-        DeviceSearchText()
     }
     if (assemblyViewModel.isShowDialog) {
         AddAssemblyDialog(navController)
