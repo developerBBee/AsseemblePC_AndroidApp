c2941f4d40a75c72bdc4c5f66af88c94700b8281
BBee <atsushi.kokubun.47@gmail.com>
2024-07-06 14:34:44 +0900

    [ver1.5] Update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "jp.developer.bbee.assemblepc"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
